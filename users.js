ae0bb34963b6affda86b5e1339d4370fbc1c6132
Felipe Soto C <pipesoto13@gmail.com>
2020-06-28 17:18:28 -0500

    "Se crea archivo users.js"

---
+++ users.js
@@ -0,0 +1,15 @@
+var User = function (name, lastName, age) {
+    this.name = name;
+    this.lastName = lastName;
+    this.age = age;
+};
+
+User.prototype.introduction = function() {
+    return ("Me llamo " + this.name + " " + this.lastName + " y tengo " + this.age);
+};
+
+var nayib = new User("Nayib", "Abdala", 34);
+var german = new User("German", "Escobar", 38);
+
+console.log(nayib.introduction());
+console.log(german.introduction());
